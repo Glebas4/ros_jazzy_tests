a742c420d258dcf507f36f227b97844cc809e188
Glebas4 <gleb.gurov.jr@gmail.com>
2025-10-02 19:56:42 +0300

    Update sub.py

--- sub.py
+++ sub.py
@@ -2,23 +2,17 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32
 
-answer = {}
-
 
 class subscriber(Node):
     def __init__(self, node_name):
         super().__init__(node_name)
-        #self.count = 0
-        #self.topics = 0
 
     def subscribe(self, topic_name):
         self.topic_name = topic_name
-        self.subscription = self.create_subscription(Int32, self.topic_name, self.listener_callback, 10)
+        self.subscription = self.create_subscription(Int32, self.topic_name, self.listener_callback, 1)
         self.subscription  # prevent unused variable warning
 
-
     def listener_callback(self, msg):
-        #self.count += 1
         val = msg.data
         if -1000 <= val and val <= 1000:
             val = val
@@ -26,34 +20,23 @@ class subscriber(Node):
             val = "ERROR"
 
         print(self.topic_name[8:], val)
-        #answer[self.topic_name] = val
         self.destroy_subscription(self.subscription)
-        #if self.topics >= self.count:
-            #for key, val in answer.items():
-            #    print(key, val)
-            #rclpy.shutdown()
+        return True
 
 
 def main(args=None):
     rclpy.init(args=None)
-    #sub = subscriber("/sensor/smoke_sensor")
     sub = subscriber("subs")
-    
     
     topic_list = sub.get_topic_names_and_types()
     sensor_topics = [topic for topic, types in topic_list if topic.startswith('/sensor/')]
     sub.topics = len(sensor_topics)
-    #for n in range(len(sensor_topics)):
-        #sensor_topics[n] = sensor_topics[n][8:]
 
-    #print(sensor_topics)
     for topic in sensor_topics:
         print(topic)
-        sub.subscribe(topic)
-    
-
-    rclpy.spin(sub)
-
+        result = sub.subscribe(topic)
+        while not result:
+            result = sub.subscribe(topic)
 
 if __name__ == '__main__':
     main()
