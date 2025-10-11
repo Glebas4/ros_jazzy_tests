9b0f9bcd806bcf1e64ca9792f17984282c9dc1d3
Glebas4 <gleb.gurov.jr@gmail.com>
2025-10-11 19:44:34 +0300

    Update platform_land.py

--- platform_land.py
+++ platform_land.py
@@ -1,4 +1,4 @@
-import rospy  # type: ignore
+import rospy
 from clover import srv 
 from std_srvs.srv import Trigger
 from aruco_pose.msg import MarkerArray
@@ -20,6 +20,16 @@ num = 0
 coords = [[1, 0.5], [1, 2], [3, 2], [3, 0.5]]
 
 
+def navigate_wait(x=0, y=0, z=0, yaw=float('nan'), speed=0.5, frame_id='aruco_map', auto_arm=False, tolerance=0.2):
+    navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
+
+    while not flag:
+        telem = get_telemetry(frame_id='navigate_target')
+        if math.sqrt(telem.x ** 2 + telem.y ** 2 + telem.z ** 2) < tolerance:
+            break
+        rospy.sleep(0.2)
+
+
 def marker_callback(msg):
     global flag
     markers = msg.markers
@@ -36,24 +46,24 @@ def range_callback(msg):
 
 
 def start():
-    navigate(x=0, y=0, z=1.7, speed=1, frame_id='body', auto_arm=True)
+    navigate_wait(x=0, y=0, z=1.8, speed=1, frame_id='body', auto_arm=True)
     rospy.sleep(3)
-    navigate(x=2, y=1.5, z=1.7, speed=1, frame_id='aruco_map')
+    navigate_wait(x=2, y=1.5, z=1.8)
     
 
-def main(args=None):
+def main():
     global z, num
     print("Start")
     start()
-    while dist > 0.25:
+    while dist > 0.3:
         if not flag:
+            z = 1.8
             cx = coords[num][0]
             cy = coords[num][1]
             num+=1
-            if num = 3:
+            if num == 3:
                 num = 0
-            navigate(x=cx, y=cy, z=1.7, speed=1, frame_id='aruco_map')
-            rospy.sleep(5)
+            navigate_wait(x=cx, y=cy, z=1.8)
         else:
             z -= 0.05
             set_position(x=0, y=0, z=z, frame_id='aruco_157')
