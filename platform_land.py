3365613e80edbf53e9c8e8159c0f11fa78e9237c
Glebas4 <gleb.gurov.jr@gmail.com>
2025-10-11 15:09:23 +0300

    Update platform_land.py

--- platform_land.py
+++ platform_land.py
@@ -20,16 +20,6 @@ num = 0
 coords = [[1, 0.5], [1, 2], [3, 2], [3, 0.5]]
 
 
-def navigate_wait(x=0, y=0, z=0, yaw=float('nan'), speed=0.5, frame_id='aruco_map', auto_arm=False, tolerance=0.4):
-    navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
-
-    while not rospy.is_shutdown():
-        telem = get_telemetry(frame_id='navigate_target')
-        if math.sqrt(telem.x ** 2 + telem.y ** 2 + telem.z ** 2) < tolerance:
-            break
-        rospy.sleep(0.2)
-
-
 def marker_callback(msg):
     global flag
     markers = msg.markers
@@ -48,8 +38,8 @@ def range_callback(msg):
 def start():
     navigate(x=0, y=0, z=1.7, speed=1, frame_id='body', auto_arm=True)
     rospy.sleep(3)
-    navigate_wait(x=2, y=1.5, z=1.7)
-
+    navigate(x=2, y=1.5, z=1.7, speed=1, frame_id='aruco_map')
+    
 
 def main(args=None):
     global z, num
@@ -60,7 +50,10 @@ def main(args=None):
             cx = coords[num][0]
             cy = coords[num][1]
             num+=1
-            navigate_wait(x=cx, y=cy, z=1.7)
+            if num = 3:
+                num = 0
+            navigate(x=cx, y=cy, z=1.7, speed=1, frame_id='aruco_map')
+            rospy.sleep(5)
         else:
             z -= 0.05
             set_position(x=0, y=0, z=z, frame_id='aruco_157')
